31c14dd44860ee0ea8d469619e60ec320d602143
QuangAnh <quanganh23122006@gmail.com>
2023-02-26 10:19:23 +0700

    add toggle

--- index.js
+++ index.js
@@ -1,26 +1,33 @@
-const List= document.getElementsByClassName("button");
+const elements= document.getElementsByClassName("button");
 const result = document.getElementById("result");
+// let ChangeTheme = true;
+let changeTheme = document.getElementById("ChangeTheme")
 
-console.log(List);
-
-for(let number of List){
+console.log(elements);
+for(let number of elements){
     number.addEventListener('click', function(){
         result.innerHTML += this.value;
     })
 }
 
 
-let Equal = () => {
+const Equal = () => {
     let res = result.innerHTML;
     let answer = eval(res);
     result.innerHTML = answer;
 }
 
-let Clear = () => {
+const Clear = () => {
     result.innerHTML = " ";
 }
 
-let Delete = () => {
+const Delete = () => {
     let string = result.innerHTML;
     result.innerHTML = string.substring(0, string.length-1);
+}
+
+const ChangeTheme = () =>{
+    for(var i = 0; i < elements.length; i++){
+        elements[i].classList.toggle("dark_mode");
+    }
 }
